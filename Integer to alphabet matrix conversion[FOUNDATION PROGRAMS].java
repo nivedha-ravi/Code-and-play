5a0b4f71430e975bd0360c8a57d7f3727005bba6
Nivedha R <63157339+nivedha-ravi@users.noreply.github.com>
2020-10-18 20:27:22 +0530

    Create Integer to alphabet matrix conversion[FOUNDATION PROGRAMS].java

---
+++ Integer to alphabet matrix conversion[FOUNDATION PROGRAMS].java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int r=in.nextInt();
+		int c=in.nextInt();
+		for(int i=0;i<r;i++,System.out.println())
+		{
+		    for(int j=0;j<c;j++)
+		    {
+		        int num=in.nextInt();
+		        System.out.print((char)(num+96)+" ");
+		    }
+		}
+
+	}
+}
