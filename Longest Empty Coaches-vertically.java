99bbe8821910c2632c8f0f31dbfd520286df7b1b
Nivedha R <63157339+nivedha-ravi@users.noreply.github.com>
2021-01-02 12:02:50 +0530

    Update Longest Empty Coaches-vertically.java

--- Longest Empty Coaches-vertically.java
+++ Longest Empty Coaches-vertically.java
@@ -87,3 +87,49 @@ public class Hello {
 		System.out.print(maxlen);
 	}
 }
+
+
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int R=scanner.nextInt();
+		int C=scanner.nextInt();
+		int[][] arr=new int[R][C];
+		for(int row=0;row<R;row++)
+		{
+		    for(int col=0;col<C;col++)
+		    {
+		        arr[row][col]=scanner.nextInt();
+		    }
+		}
+		int col=0;
+		int max=0,sum=0;
+		while(col<C)
+		{
+		for(int row=0;row<R;row++)
+		{
+		   if(arr[row][col]==0)
+		   sum++;
+		   else
+		   sum=0;
+		   if(sum>max)
+		   max=sum;
+		}
+		col++;
+		if(col<C)
+		for(int row=R-1;row>=0;row--)
+		{
+		    if(arr[row][col]==0)
+		    sum++;
+		    else
+		    sum=0;
+		    if(sum>max)
+		    max=sum;
+		}
+		col++;
+		}
+		System.out.print(max);
+	}
+}
